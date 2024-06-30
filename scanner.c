4cc4c512aac89fe9668b32403f54291cfa6a4922
Maple <wang-jiaxun@outlook.com>
2024-06-30 18:22:59 +0800

    add char scanning

--- scanner.c
+++ scanner.c
@@ -1,5 +1,6 @@
 #include "scanner.h"
 #include "common.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -17,11 +18,62 @@ void initScanner(const char* source) {
   scanner.line = 1;
 }
 
-Token scanToken(){
-  scanner.start=scanner.current;
+static bool isAtEnd() { return *scanner.current == '\0'; }
 
-  if(isAtEnd()) return makeToken(TOKEN_EOF);
+static char advance() {
+  scanner.current++;
+  return scanner.current[-1];
+}
+
+static bool match(char expected) {
+  if (isAtEnd()) return false;
+  if (*scanner.current != expected) return false;
+  scanner.current++;
+  return true;
+}
+
+static Token makeToken(TokenType type) {
+  Token token;
+  token.type = type;
+  token.start = scanner.start;
+  token.length = (int)(scanner.current - scanner.start);
+  token.line = scanner.line;
+  return token;
+}
+
+static Token errorToken(const char* message) {
+  Token token;
+  token.type = TOKEN_ERROR;
+  token.start = message;
+  token.length = (int)strlen(message);
+  token.line = scanner.line;
+  return token;
+}
+
+Token scanToken() {
+  scanner.start = scanner.current;
+
+  if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+  char c = advance();
+
+  switch (c) {
+  case '(': return makeToken(TOKEN_LEFT_PAREN);
+  case ')': return makeToken(TOKEN_RIGHT_PAREN);
+  case '{': return makeToken(TOKEN_LEFT_BRACE);
+  case '}': return makeToken(TOKEN_RIGHT_BRACE);
+  case ';': return makeToken(TOKEN_SEMICOLON);
+  case ',': return makeToken(TOKEN_COMMA);
+  case '.': return makeToken(TOKEN_DOT);
+  case '-': return makeToken(TOKEN_MINUS);
+  case '+': return makeToken(TOKEN_PLUS);
+  case '/': return makeToken(TOKEN_SLASH);
+  case '*': return makeToken(TOKEN_STAR);
+  case '!': return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+  case '=': return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+  case '<': return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+  case '>': return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+  }
 
   return errorToken("Unexpected character.");
 }
-
