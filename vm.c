86ca1881ce98168098c32a514cefd62d70869801
Maple <wang-jiaxun@outlook.com>
2024-06-30 13:36:49 +0800

    init compiler and scanner

--- vm.c
+++ vm.c
@@ -1,4 +1,5 @@
 #include "vm.h"
+#include "compiler.h"
 #include "common.h"
 #include "debug.h"
 #include <stdio.h>
@@ -24,6 +25,12 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do {                \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b);     \
+  } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -43,6 +50,10 @@ static InterpretResult run() {
       push(constant);
       break;
     }
+    case OP_ADD: BINARY_OP(+); break;
+    case OP_SUBTRACT: BINARY_OP(-); break;
+    case OP_MULTIPLY: BINARY_OP(*); break;
+    case OP_DIVIDE: BINARY_OP(/); break;
     case OP_NEGATE: push(-pop()); break;
     case OP_RETURN: {
       printValue(pop());
@@ -53,10 +64,11 @@ static InterpretResult run() {
   }
 
 #undef READ_BYTE
+#undef READ_CONSTANT
+#undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char* source) {
+  compile(source);
+  return INTERPRET_OK;
 }
--- vm.h
+++ vm.h
@@ -21,7 +21,7 @@ typedef struct {
 
 void initVM();
 void freeVM();
-InterpretResult interpret(Chunk* chunk);
+InterpretResult interpret(const char* source);
 void push(Value value);
 Value pop();
 
