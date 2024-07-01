9b8681079645567474b7142a614036537e98d913
Maple <wang-jiaxun@outlook.com>
2024-07-01 20:34:01 +0800

    add string and number scanning

--- scanner.c
+++ scanner.c
@@ -18,11 +18,20 @@ void initScanner(const char* source) {
   scanner.line = 1;
 }
 
+static bool isDigit(char c) { return c >= '0' && c <= '9'; }
+
 static bool isAtEnd() { return *scanner.current == '\0'; }
 
 static char advance() {
   scanner.current++;
   return scanner.current[-1];
+}
+
+static char peek() { return *scanner.current; }
+
+static char peekNext() {
+  if (isAtEnd()) return '\0';
+  return scanner.current[1];
 }
 
 static bool match(char expected) {
@@ -50,12 +59,68 @@ static Token errorToken(const char* message) {
   return token;
 }
 
+static void skipWhitespace() {
+  for (;;) {
+    char c = peek();
+    switch (c) {
+    case ' ':
+    case '\r':
+    case '\t': advance(); break;
+    case '\n':
+      scanner.line++;
+      advance();
+      break;
+    case '/':
+      if (peekNext() == '/') {
+        // A comment goes until the end of the line.
+        while (peek() != '\n' && !isAtEnd())
+          advance();
+      } else {
+        return;
+      }
+      break;
+    default: return;
+    }
+  }
+}
+
+static Token number() {
+  while (isDigit(peek()))
+    advance();
+
+  // Look for a fractional part.
+  if (peek() == '.' && isDigit(peekNext())) {
+    // Consume the ".".
+    advance();
+
+    while (isDigit(peek()))
+      advance();
+  }
+
+  return makeToken(TOKEN_NUMBER);
+}
+
+static Token string() {
+  while (peek() != '"' && !isAtEnd()) {
+    if (peek() == '\n') scanner.line++;
+    advance();
+  }
+
+  if (isAtEnd()) return errorToken("Unterminated string.");
+
+  // The closing quote.
+  advance();
+  return makeToken(TOKEN_STRING);
+}
+
 Token scanToken() {
+  skipWhitespace();
   scanner.start = scanner.current;
 
   if (isAtEnd()) return makeToken(TOKEN_EOF);
 
   char c = advance();
+  if (isDigit(c)) return number();
 
   switch (c) {
   case '(': return makeToken(TOKEN_LEFT_PAREN);
@@ -73,6 +138,7 @@ Token scanToken() {
   case '=': return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
   case '<': return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
   case '>': return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+  case '"': return string();
   }
 
   return errorToken("Unexpected character.");
